c8263e5ef5ded7cc263f2a4002529b7e84ae0861
Chaitanya384119 <chaitanyaagrawal384@gmail.com>
2025-12-09 05:37:05 +0000

    day2

---
+++ d2.js
@@ -0,0 +1,15 @@
+let a="abc"
+let b=""
+let c=null
+let d
+console.log("a="+a)
+console.log("b="+b)
+console.log("c="+c)
+console.log("d="+d)
+console.log(typeof(d))
+let score="33ab"
+console.log(typeof(score))
+console.log(typeof score)
+let s=Number(score)
+console.log(typeof s)
+console.log(s)
---
+++ day2.js
@@ -0,0 +1,16 @@
+let a=23
+const b="abc"
+var c=78
+d=88
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+a=49
+b="def"
+c=90
+d=99
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
