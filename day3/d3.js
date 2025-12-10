2b6c700733a786a75a58383855be29ccb62649e4
Chaitanya384119 <chaitanyaagrawal384@gmail.com>
2025-12-10 05:30:57 +0000

    day3

--- day3/d3.js
+++ day3/d3.js
@@ -20,3 +20,24 @@ arr.pop();
 for(let value of arr){
     console.log(value);
 }
+
+
+
+arr=[2,4,6,8]
+let doubled=arr.map(n=>n*2)
+for(let value of doubled){
+    console.log(value);
+}
+
+arr=[10,25,30,5,60]
+let res=arr.filter(n=>n>20)
+for(let value of res){
+    console.log(value);
+}
+
+
+
+arr=[1,2,3,4,5]
+let total=arr.reduce((acc,val)=>1+val,0)
+
+console.log(total);
